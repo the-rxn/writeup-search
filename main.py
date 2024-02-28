aaf77f03ec1c398697b2861627f3403a8aa716e3
the-rxn <coppiemertz@gmail.com>
2024-02-28 20:32:58 +0300

    feat: marqo init

---
+++ main.py
@@ -0,0 +1,27 @@
+import marqo
+
+mq = marqo.Client(url='http://localhost:8882')
+
+mq.create_index("my-first-index")
+
+mq.index("my-first-index").add_documents([
+    {
+        "Title": "The Travels of Marco Polo",
+        "Description": "A 13th-century travelogue describing Polo's travels"
+    }, 
+    {
+        "Title": "Extravehicular Mobility Unit (EMU)",
+        "Description": "The EMU is a spacesuit that provides environmental protection, "
+                       "mobility, life support, and communications for astronauts",
+        "_id": "article_591"
+    }],
+    tensor_fields=["Description"]
+)
+
+results = mq.index("my-first-index").search(
+    q="I like to travel?"
+)
+
+# let's print out the results:
+import pprint
+pprint.pprint(results)
